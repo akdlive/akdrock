d6eeeb05f72346b9c782ec6e442f59215019a9d1
akdlive <140449747+akdlive@users.noreply.github.com>
2023-07-30 15:50:44 +0530

    Add files via upload

---
+++ tttt.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Home Page</title>
+</head>
+<body>
+    <h1>Welcome to My PHP Home Page</h1>
+
+    <?php
+    // PHP code goes here (if needed)
+    ?>
+
+    <p>This is a simple PHP home page.</p>
+</body>
+</html>
